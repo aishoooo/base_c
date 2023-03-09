2d9d4d2f79e393bbb934af0370c9cc6170d60dbb
Paul Chretien <117069257+aishoooo@users.noreply.github.com>
2023-03-09 19:24:54 +0100

    Update sudoku.c
    update

--- base_c/sudoku.c
+++ base_c/sudoku.c
@@ -1,31 +1,71 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include  <sys/types.h>
-#include  <sys/stat.h>
-#include  <fcntl.h>
-
-
+#include "includes.h"
 
 int main(int argc, char **argv){
-    char buf [91];
-    int grid [9][9];
-    int grid_total;
+    char buf[91];
+    int **board;
+    int *ret;
+    int i = 0;
+    int j = 0;
+    int m = 0;
+    int n = 0;
+    int free_case = 0;
 
-    grid_total = grid;
+    board = (int **)malloc(sizeof(int *)*9);
 
+    while(i < 9){
+        board[i] = (int *)malloc(sizeof(int)*9);
+        i++;
+    }
+    
     int fd = open(argv[1], O_RDONLY);
     int nb_read = read(fd,buf, 90);
 
-    char *ret = (char*)malloc(sizeof(char) *grid);
-
-
     read(fd, buf, 90);
     printf("%i\n",argc);
-    printf("%s\n",buf);
+    // printf("%s\n",buf);
     printf("%i\n", nb_read);
-    printf("%s\n",ret);
-    printf("%s\n",grid_total);
     buf[90]= '\0';
     close(fd);
+
+    while(buf[j] != '\0') {
+        board[n][m] = buf[j]-48;
+        //if(board[n][m] != '0') {
+        //   free_case ++;
+        }
+        if(buf[j] == '\n') {
+            m = 0;
+            n++;
+            j++ ;
+        } else {
+            m++;
+            j++;
+            
+        }
+    }
+    //printf("%i\n",free_case);
+
+    n = 0;
+    m = 0;
+    while (m < 9){
+        while(n < 9){
+            printf("%i   ",board[m][n]);
+            n++;
+        }
+        m++;
+        n = 0;
+        printf("%c \n", '\n');
+
+    }
+
+    
+
+    i = 0;
+    while(i < 9) {
+        free(board[i]);
+        i++;
+    }
+    free(board);
+
+    return 0;
+
 }
